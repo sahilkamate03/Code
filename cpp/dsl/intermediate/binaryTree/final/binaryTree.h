77f5c1eeea0eea2914b69124654c3e341508594b
SahilKamate <sahilkamate03@gmail.com>
2023-03-30 13:15:22 +0530

    30-03-2023

---
+++ cpp/dsl/intermediate/binaryTree/final/binaryTree.h
@@ -0,0 +1,59 @@
+#ifndef BINARYTREE_H
+#define BINARYTREE_H
+
+class Node
+{
+private:
+    int data;
+    Node* left;
+    Node* right;
+
+public:
+    Node(): data{0}, left{nullptr}, right{nullptr} {}
+    Node(int data): data{data}, left{nullptr}, right{nullptr} {}
+
+    int getData() const {return data;}
+    Node* getLeft() const {return left;}
+    Node* getRight() const {return right;}
+
+    friend class BinaryTree;
+    friend class BinarySearchTree;
+    friend Node* cloneBinaryTree(Node*);
+    friend Node* mirrorBinaryTree(Node* root);
+};
+
+class BinaryTree
+{
+private:
+    Node* root;
+
+public:
+    BinaryTree(): root{nullptr} {}
+
+    Node* getRoot() const {return root;}
+    void setRoot(Node* newRoot) {root=newRoot;}  
+
+    void insert(int);
+
+    void preOrderTraversal();
+    void inOrderTraversal();
+    void postOrderTraversal();
+
+    int height(Node*);
+    friend class BinarySearchTree;
+};
+
+
+class BinarySearchTree : public BinaryTree
+{
+public:
+    void insert(int);
+    bool search(int);
+    void remove(int);
+};
+
+
+bool compareBinaryTree(Node*, Node*);
+Node* cloneBinaryTree(Node*);
+
+#endif
