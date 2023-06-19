338603a2f2f9b5dcd02b43b82a3d6563e6e9a0c2
sahilkamate03 <sahilkamate03@gmail.com>
2023-06-19 15:16:52 +0530

    2023-06-19 -ab

--- cpp/leetcode/pow.cpp
+++ cpp/leetcode/pow.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 double power(float x, long long int n)
 {
-	if (n==1)
-		return x;
-	double s =power(x, n/2);
-	if (n&1)
-		return s*s*x;
-	else return s*s;
+    if (n==1)
+	return x;
+    double s =power(x, n/2);
+    if (n&1)
+	return s*s*x;
+    else return s*s;
 }
 
 
 int main()
 {
-	float x; long long int n;
-	cin >> x>> n;
-	if (n==0)
-		cout << 1 << endl;
-	else if (n<0)
-		cout << 1/power(x, n) << endl;
-	else 
-		cout << power(x,n) << endl;
-	return 0;
+    float x; long long int n;
+    cin >> x>> n;
+    if (n==0)
+	cout << 1 << endl;
+    else if (n<0)
+    cout << 1/power(x, n) << endl;
+    else 
+	cout << power(x,n) << endl;
+    return 0;
 }
